97e2d24d95aad48d252b8d39deff6729d54cd324
Sean Daly <senorcrankypants@gmail.com>
2022-09-20 00:13:42 -0700

    basic cards, deck, with test of drawing a deck and a hand

--- sigil.py
+++ sigil.py
@@ -11,11 +11,15 @@ class Card:
         self.sigils = sigils
 
     def __str__(self):
-        return '%s %x %x' % (self.name, self.attack, self.health)
+        return self.log()
 
     def __repr(self):
-        return '%s %x %x' % (self.name, self.attack, self.health)
+        return self.log()
     
+    def log(self):
+        #return '<Card: %s a:%x h:%x>' % (self.name, self.attack, self.health)
+        return '<Card: %s>' % (self.name)
+
 class CardSet:
     # Base class for all groups of cards (full deck, player deck, hand, etc)
 
@@ -23,9 +27,16 @@ class CardSet:
         self.cards = cards
 
     def __str__(self):
+        return self.log()
+
+    def __repr(self):
+        return self.log()
+    
+    def log(self):
         ret = ''
         for c in self.cards:
-           ret += str(c)
+            ret += str(c)
+        return "[%s]" % ret
 
 class FullDeck(CardSet):
     # Child Class of Cardset to represent the full deck of available cards
@@ -41,6 +52,7 @@ class FullDeck(CardSet):
         retSet = CardSet([]);
         for x in range(n):
             i = random.randrange(0, len(self.cards))
+            print("drawing random card: %s" % self.cards[i])
             retSet.cards.append(self.cards[i])
         return retSet 
 
@@ -48,8 +60,16 @@ class FullDeck(CardSet):
 class PlayerDeck(CardSet):
     # Child class of Cardset to represent the players deck
 
-    def __init__(self, cards):
+    def __init__(self, cards): 
         self.cards = cards
+
+    def draw(self, n):
+        # draw hand from the player's deck take top n
+        retSet = CardSet(self.cards[:n]);
+        # remove those cards from player deck
+        self.cards = self.cards[n:]
+        return retSet 
+
 
 class Hand(CardSet):
     # Child class of Cardset to represent the players hand
@@ -61,11 +81,23 @@ class Game:
     lanes = 3
     deck = {}
     playerDeck = {}
+    hand = {}
 
     def __init__(self):
         deck = FullDeck()
-        playerDeck = deck.draw(6)
-        print(playerDeck)
+        print()
+        print("Full Deck: %s" % deck)
+        print()
+        playerDeck = PlayerDeck(deck.draw(6).cards)
+        print("Player Deck: %s" % playerDeck)
+        print()
+        print("now drawing a hand of 3")
+        hand = playerDeck.draw(3);
+        print("hand: %s" % hand)
+        print()
+        print("Player Deck: %s" % playerDeck)
+        print()
+
 
 game = Game()
 
